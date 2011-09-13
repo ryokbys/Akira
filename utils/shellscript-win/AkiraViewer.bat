7b32564b3d7e7793316efd9e49787034a7c090b2
NAKAMURA Takahide <nkmrtkhd@gmail.com>
2011-09-13 18:22:27 +0900

    bugfix script for windows

--- utils/shellscript-win/AkiraViewer.bat
+++ utils/shellscript-win/AkiraViewer.bat
@@ -1,1 +1,1 @@
-java -Xmx1024m -cp \jogl2\gluegen-gl.jar;\jogl2\gluegen-rt.jar;\jogl2\jogl.all.jar;\jogl2\newt.all.jar;\jogl2\nativewindow.all.jar;\akira\Akira.jar viewer.AkiraViewer
+java -Xmx1024m -cp \jogl2\gluegen-gl.jar;\jogl2\gluegen-rt.jar;\jogl2\jogl.all.jar;\jogl2\newt.all.jar;\jogl2\nativewindow.all.jar;\Akira\Akira.jar viewer.AkiraViewer
